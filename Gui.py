885d6716386fd2ae3e7b38d5b418a83f33086ecb
Deuter <wojciechbernat95@gmail.com>
2020-07-30 21:04:22 +0200

    Implemented created lines

--- Gui.py
+++ Gui.py
@@ -11,8 +11,30 @@ class Gui(QWidget):
         super().__init__(parent)
 
         self.createInterface()
-        self.createLabels()
         self.show()
+
+    def createLineEdit(self):
+        #create line edit
+        self.SerialPortLine = QLineEdit()
+        self.TemperatureLine = QLineEdit()
+        ## Here is a place for add ne label
+        ##
+        self.LogLine = QLineEdit()
+
+        #line settings
+        self.SerialPortLine.setReadOnly(True)
+        self.TemperatureLine.setReadOnly(True)
+        self.LogLine.setReadOnly(True)
+
+        #line tool tip
+        self.SerialPortLine.setToolTip('Serial port information.')
+        self.TemperatureLine.setToolTip('Tank CPU temperature.')
+        self.LogLine.setToolTip('Program logs.')
+
+        self.layOutPattern.addWidget(self.SerialPortLine, 1, 0)
+        self.layOutPattern.addWidget(self.TemperatureLine, 1, 3)
+        self.layOutPattern.addWidget(self.LogLine, 4, 0)
+        
 
     def createLabels(self):
         #lables
@@ -21,15 +43,16 @@ class Gui(QWidget):
         self.LogsLabel = QLabel("Logs: ", self)
 
         #connect labels to layout
-        self.labelsLayout = QGridLayout()
-        self.labelsLayout.addWidget(self.SerialPortLabel, 0, 0)
-        self.labelsLayout.addWidget(self.TemperatureLabel, 0, 3)
-        self.labelsLayout.addWidget(self.LogsLabel, 3, 0)
+        self.layOutPattern.addWidget(self.SerialPortLabel, 0, 0)
+        self.layOutPattern.addWidget(self.TemperatureLabel, 0, 3)
+        self.layOutPattern.addWidget(self.LogsLabel, 3, 0)
 
-        self.setLayout(self.labelsLayout)
+        self.setLayout(self.layOutPattern)
 
     def createInterface(self):
-
+        self.layOutPattern = QGridLayout()
+        self.createLabels()
+        self.createLineEdit()
 
         #windows settings
         self.setGeometry(200, 200, 300, 100)
